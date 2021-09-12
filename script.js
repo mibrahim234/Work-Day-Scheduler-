ad6e749b3cdfb6cff7bb2408d0f67c28d89a1886
mibrahim234 <mohamm1417@gmail.com>
2021-09-12 18:26:44 -0400

    logic draft

--- script.js
+++ script.js
@@ -18,3 +18,66 @@ hoursArray.forEach(function(hour) {
     rowDiv.append(timeColomn, textColomn, btnCol);
     $('container').append(rowDiv);
 })
+
+//setter
+moment().seconds(30);
+
+//getter
+moment().seconds();
+
+
+//pass in milliseconds
+moment.duration (3600).humanize();
+
+//specify what kind of time measurement
+moment.duration(3600, 'seconds').hours();
+
+
+//languages
+moment.locale('es');
+var march = moment ('2017-03')
+// console.log(march.format('MMMM'))
+
+
+// var currentDayEl = $('#currentDay');
+// var currentDay = moment().format('MMMM Do YYYY');
+// currentDayEl.text(currentDay);
+
+
+var currentTime =  moment().hours();
+
+$('#currentDay').text(moment().format('MMMM Do YYYY'));
+
+
+function saveDescription () {
+    var time = $(this).parent().attr('id');
+    var descriptionText = $(this).siblings('.description').val();
+    localStorage.setItem(time, descriptionText);
+}
+
+
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+
+
+
+$('.time-block').each(function(){
+    var hourBlock = parseInt($(this).attr('id')) 
+
+    if ( hourBlock < currentTime ) {
+        $(this).addClass('past')
+    } else if ( hourBlock === currentTime ) {
+        $(this).removeClass('past')
+        $(this).addClass('present')
+    } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+})
+
+
+
+
+
+$('.saveBtn').click(saveDescription);
