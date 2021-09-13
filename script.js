6f98d39f15e8de4492858713d14b96016e0ee06a
mibrahim234 <mohamm1417@gmail.com>
2021-09-12 21:19:59 -0400

    draft

--- script.js
+++ script.js
@@ -22,67 +22,28 @@ hoursArray.forEach(function(hour) {
 
 //sets to local storage
 //val() method returns or sets the value attribute of the selected elements
-
-function saveDescription () {
-    var time = $(this).parent().attr('id');
-    var desText = $(this).siblings('.description').val();
-    localStorage.setItem(time, desText);
-}
-
-//gets from local storage
-hoursArray.forEach(function(hour) {
-    $(`#${hour} .description`).val(localStorage.getItem(hour));
-})
-//setter
-moment().seconds(30);
-
-//getter
-moment().seconds();
-
-
-//pass in milliseconds
-moment.duration (3600).humanize();
-
-//specify what kind of time measurement
-moment.duration(3600, 'seconds').hours();
-
-
-//languages
-moment.locale('es');
-var march = moment ('2017-03')
-// console.log(march.format('MMMM'))
-
-
-// var currentDayEl = $('#currentDay');
-// var currentDay = moment().format('MMMM Do YYYY');
-// currentDayEl.text(currentDay);
-
-
-var currentTime =  moment().hours();
-
-$('#currentDay').text(moment().format('MMMM Do YYYY'));
-
-
 function saveDescription () {
     var time = $(this).parent().attr('id');
     var descriptionText = $(this).siblings('.description').val();
     localStorage.setItem(time, descriptionText);
 }
 
+//gets from local storage
+hoursArray.forEach(function(hour) {
+    $(`#${hour} .description`).val(localStorage.getItem(hour));
+})
 
-$('#9 .description').val(localStorage.getItem('9'));
-$('#10 .description').val(localStorage.getItem('10'));
-
-
-
+//adds css classes to hour rows depending on if its in the present past or future
 $('.time-block').each(function(){
-    var hourBlock = parseInt($(this).attr('id')) 
-
-    if ( hourBlock < currentTime ) {
-        $(this).addClass('past')
-    } else if ( hourBlock === currentTime ) {
-        $(this).removeClass('past')
-        $(this).addClass('present')
+    var hourBlock = parseInt($(this).attr('id'));
+    if (hourBlock < currentHour) {
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass('past');
+    } else if (hourBlock === currentHour) {
+        $(this).removeClass('future');
+        $(this).removeClass('past');
+        $(this).addClass('present');
     } else {
         $(this).removeClass('past');
         $(this).removeClass('present');
@@ -91,7 +52,13 @@ $('.time-block').each(function(){
 })
 
 
-
-
-
 $('.saveBtn').click(saveDescription);
+console.log(saveDescription);
+
+//clear button event listener
+$('.clearBtn').click(function(){
+    localStorage.clear();
+    hoursArray.forEach(function(hour) {
+        $(`#${hour} .description`).val('');
+    })
+})
